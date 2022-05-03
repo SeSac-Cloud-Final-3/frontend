e7c8e2897955f93b86d5cada45ed378ec497eba3
onzero98 <dev_210@naver.com>
2022-05-03 10:11:25 +0900

    대회 자본 가치 reRender when competition changes

--- src/views/stock/Ranking.js
+++ src/views/stock/Ranking.js
@@ -21,7 +21,7 @@ const Ranking = ({ backAPI, competition }) => {
     return () => {
       isMounted = false;
     };
-  }, []);
+  }, [competition]);
 
   const getRanking = async (request) => {
     let ranking = [];
